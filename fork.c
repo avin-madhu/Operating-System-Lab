f6c429cfed35a63b074f4a2567f06f0a5d1fc5f9
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2023-03-14 18:45:59 +0530

    Create

---
+++ fork.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int n;
+     
+    printf("Enter the Limit: ");
+    scanf("%d",&n);
+    int check = fork();
+
+    if(check>0) // parent process
+    {
+        printf("Even numbers upto %d: ",n);
+        for(int i = 0;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                printf("%d ",i);
+            }
+        }
+    }
+    if(check==0)  // child process
+    {
+        
+        printf("\nOdd numbers upto %d: ",n);
+        for(int i=0;i<n;i++)
+        {
+            if(i%2!=0)
+            {
+                printf("%d ",i);
+            }
+        }
+    }
+
+    return 0;
+}
