572dea641c6c3c98bf5da9d46f886017d6e84bb8
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2023-08-19 18:29:49 +0530

    prio

--- CPUSched/ps.c
+++ CPUSched/ps.c
@@ -1,82 +1,66 @@
 #include <stdio.h>
+int main()
+{
+    int n,bt[10],p[10],pro[10],wt[10],tat[10];
+    float awt=0,atat=0;
+    printf("Enter the number of processes: ");
+    scanf("%d",&n);
 
-int i, j, n;
-float tatAvg, wtAvg;
-
-struct Process {
-    int pId;
-    int bt;
-    int priority;
-};
-
-void read(struct Process p[]) {
-    printf("\n");
-    printf("Enter the Burst Time, Priority Of Each Process :\n");
-    printf("\n");
-    for (i = 0; i < n; ++i) 
+    for(int i=0;i<n;i++)
     {
-        p[i].pId = i + 1;
-        printf("Process %d: ", p[i].pId);
-        scanf("%d %d", &p[i].bt, &p[i].priority);
-
+        pro[i]= i+1;
     }
-}
 
-void display(struct Process p[], int wt[], int tat[]) {
-    printf("\n-------------------------------------------------------------------");
-    printf("\nProcessID | BurstTime | Priority | Waiting time | Turn Around Time\n");
-    printf("---------------------------------------------------------------------\n");
-    for (i = 0; i < n; ++i) {
-        printf("%5d\t\t%d\t%4d\t\t%d\t\t%d\n", p[i].pId, p[i].bt, p[i].priority, wt[i], tat[i]);
+    printf("Enter the burst and priority of each process: \n");
+
+    for(int i=0; i<n; i++)
+    {
+      printf("P%d: ",i+1);
+      scanf("%d %d",&bt[i],&p[i]);
     }
-    printf("\nAverage Waiting Time: %.3f", wtAvg);
-    printf("\naverage Turnaround Time: %.3f", tatAvg);
-    printf("\n");
-}
 
-void sort(struct Process p[]) {
-    struct Process temp;
-    for (i = 0; i < n - 1; ++i) {
-        for (j = 0; j < n - 1 - i; ++j) {
-            if (p[j].priority > p[j + 1].priority) {
-                temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
+    // sorting according to the priority
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=0; j<n-i-1; j++)
+        {
+            if(p[j]>p[j+1])
+            {
+                int temp = p[j+1];
+                p[j+1] = p[j];
+                p[j] = temp;
+
+                temp = pro[j+1];
+                pro[j+1] = pro[j];
+                pro[j] = temp;
+
+                temp = bt[j+1];
+                bt[j+1] = bt[j];
+                bt[j] = temp;
             }
         }
     }
-}
-
-void findWaitingTime(struct Process p[], int wt[]) {
+   
     wt[0] = 0;
-    int wtSum = 0;
-    for (i = 1; i < n; ++i) {
-        wt[i] = wt[i - 1] + p[i - 1].bt;
-        wtSum += wt[i];
+
+    printf("Pr\tBT\tPrio\tWT\tTAT\n");
+
+    for(int i=0;i<n;i++)
+    {
+       wt[i] =0;
+       tat[i] = 0;
+       for(int j=0;j<i;j++)
+       {
+          wt[i] += bt[j];
+       }
+
+       tat[i] = wt[i] + bt[i] ;
+       awt += wt[i];
+       atat += tat[i];
+
+       printf("%d\t%d\t%d\t%d\t%d\n",pro[i],bt[i],p[i],wt[i],tat[i]);
     }
-    wtAvg = (float)wtSum / n;
-}
 
-void findTurnAroundtime(struct Process p[], int tat[], int wt[]) {
-    int tatSum = 0;
-    for (i = 0; i < n; ++i) {
-        tat[i] = p[i].bt + wt[i];
-        tatSum += tat[i];
-    }
-    tatAvg = (float)tatSum / n;
-}
-
-int main()
-{
-    struct Process p[20];           /* Array object for the structure */
-    int wt[20], tat[20];
-    printf("Enter Number of Processes:");
-    scanf("%d", &n);
-    read(p);
-    sort(p);
-    findWaitingTime(p, wt);
-    findTurnAroundtime(p, tat, wt);
-    display(p, wt, tat);
-
-    return 0;
+    printf("\nAverage waiting time: %f\n",awt/n);
+    printf("Average turnaround time: %f",atat/n);
 }
